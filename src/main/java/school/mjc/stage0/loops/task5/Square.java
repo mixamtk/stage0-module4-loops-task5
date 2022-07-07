511e6f9b74163585014e13ba6bfecc4ba49648b7
mixamtk <70889784+mixamtk@users.noreply.github.com>
2022-07-07 11:48:59 +0300

    Update Square.java

--- src/main/java/school/mjc/stage0/loops/task5/Square.java
+++ src/main/java/school/mjc/stage0/loops/task5/Square.java
@@ -2,6 +2,23 @@ package school.mjc.stage0.loops.task5;
 
 public class Square {
     public void printSquareFrom8s(int sideLength){
-
+            for (int i = 1; i <= sideLength; i++) {
+                for (int a = 1; a <= sideLength; a++) {
+                    if (i == 1 || i == sideLength) {
+                        if (a == sideLength)
+                            System.out.println("8");
+                        else
+                            System.out.print("8");
+                    } else {
+                        if (a == 1 )
+                            System.out.print("8");
+                        else
+                        if (a > 1 && a < sideLength)
+                            System.out.print(" ");
+                        else
+                            System.out.println("8");
+                    }
+                }
+            }
     }
 }
