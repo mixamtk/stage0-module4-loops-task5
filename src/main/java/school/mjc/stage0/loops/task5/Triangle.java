a08baafbf9431426ab9455afa0a062927933af99
mixamtk <70889784+mixamtk@users.noreply.github.com>
2022-07-08 12:13:09 +0300

    Update Triangle.java

--- src/main/java/school/mjc/stage0/loops/task5/Triangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Triangle.java
@@ -5,6 +5,9 @@ public class Triangle {
         for (int i = 1; i <= cathetusLength; i++) {
             for (int a = 1; a <= cathetusLength; a++) {
                 if (a <= i) {
+                    if (i == cathetusLength && a == i)
+                        System.out.println(8);
+                    else
                     System.out.print("8");
                 } else if (a == cathetusLength) {
                     System.out.println(" ");
